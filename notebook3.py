09f98ca3c602f956fe95d3346bdf3041261c6e93
Natashatschz07 <yoyinsanchezc@gmail.com>
2025-09-13 16:20:32 -0400

    Add files via upload

--- notebook3.py
+++ notebook3.py
@@ -119,69 +119,73 @@ frame_doctores=ttk.Frame(pestañas)
 pestañas.add(frame_doctores,text="Doctores")
 pestañas.pack(expand=True, fill="both")
 
+labelRegistro=tk.Label(frame_doctores,text="Registro de Doctores")
+labelRegistro.grid(row=0,column=0,sticky="w",padx=5,pady=5)
+
+
 #Nombre
 labelNombr=tk.Label(frame_doctores,text="Nombre :")
-labelNombr.grid(row=0,column=0,padx=5,pady=5,sticky="w")
-nombrP=tk.Entry(frame_doctores)
-nombrP.grid(row=0,column=1,padx=5,pady=5,sticky="w")
+labelNombr.grid(row=1,column=0,padx=5,pady=5,sticky="w")
+nombrD=tk.Entry(frame_doctores)
+nombrD.grid(row=1,column=1,padx=5,pady=5,sticky="w")
 
 #Especialidad
 labelEspecialidad=tk.Label(frame_doctores,text="Especialidad:")
-labelEspecialidad.grid(row=1,column=0,sticky="w",padx=5,pady=5)
+labelEspecialidad.grid(row=2,column=0,sticky="w",padx=5,pady=5)
 especialidad=tk.StringVar()
 especialidad.set("Cardiología")#Valor por defecto
 comboEspecialidad=ttk.Combobox(frame_doctores,values=["Cardiología","Pediatría","Neurología","Traumatología"],textvariable=especialidad)
-comboEspecialidad.grid(row=1,column=1,sticky="w",padx=5,pady=5)
+comboEspecialidad.grid(row=2,column=1,sticky="w",padx=5,pady=5)
 
 #Edad
-labelEdad=tk.Label(frame_doctores,text="Edad")
-labelEdad.grid(row=3,column=0,padx=5,pady=5,sticky="w")
+labelEdadD=tk.Label(frame_doctores,text="Edad")
+labelEdadD.grid(row=3,column=0,padx=5,pady=5,sticky="w")
 spin=tk.Spinbox(frame_doctores,from_=1,to=100)
 spin.grid(row=3,column=1,padx=5,pady=5,sticky="w")
 
 #Teléfono
 labelTelef=tk.Label(frame_doctores,text="Teléfono:")
 labelTelef.grid(row=4,column=0,padx=5,pady=5,sticky="w")
-telefP=tk.Entry(frame_doctores)
-telefP.grid(row=4,column=1,padx=5,pady=5,sticky="w")
+telefD=tk.Entry(frame_doctores)
+telefD.grid(row=4,column=1,padx=5,pady=5,sticky="w")
 
 
 #Frame para los botones
-btn_frame=tk.Frame(frame_doctores)
-btn_frame.grid(row=9,column=0, columnspan=2, pady=5,sticky="w")
+btn_frameD=tk.Frame(frame_doctores)
+btn_frameD.grid(row=9,column=0, columnspan=2, pady=5,sticky="w")
 
 #Boton Registrar
-btn_registrar=tk.Button(btn_frame,text="Registrar", command="",bg="LimeGreen")
-btn_registrar.grid(row=0,column=0, padx=5)
+btn_registrarD=tk.Button(btn_frameD,text="Registrar", command="",bg="LimeGreen")
+btn_registrarD.grid(row=0,column=0, padx=5)
 
 #Botn Eliminar
-btn_eliminar=tk.Button(btn_frame, text="Eliminar", command="",bg="Tomato")
-btn_eliminar.grid(row=0,column=1, padx=5)
+btn_eliminarD=tk.Button(btn_frameD, text="Eliminar", command="",bg="Tomato")
+btn_eliminarD.grid(row=0,column=1, padx=5)
 
 #Crear treeView para mostrar pacientes
-treeview=ttk.Treeview(frame_doctores,columns=("Nombre","Especialidad","Edad","Telefono"),show="headings")
+treeviewD=ttk.Treeview(frame_doctores,columns=("Nombre","Especialidad","Edad","Telefono"),show="headings")
 
 #Definir encabezados
-treeview.heading("Nombre",text="Nombre")
-treeview.heading("Especialidad",text="Especialidad")
-treeview.heading("Edad",text="Edad")
-treeview.heading("Telefono",text="Teléfono")
+treeviewD.heading("Nombre",text="Nombre")
+treeviewD.heading("Especialidad",text="Especialidad")
+treeviewD.heading("Edad",text="Edad")
+treeviewD.heading("Telefono",text="Teléfono")
 
 
 #Definir anchos de columnas
-treeview.column("Nombre", width=120)
-treeview.column("Especialidad", width=120)
-treeview.column("Edad", width=100, anchor="center")
-treeview.column("Telefono", width=100, anchor="center")
+treeviewD.column("Nombre", width=120)
+treeviewD.column("Especialidad", width=120)
+treeviewD.column("Edad", width=100, anchor="center")
+treeviewD.column("Telefono", width=100, anchor="center")
 
 
 #Ubicar el TreeView en la cuadrícula
-treeview.grid(row=10,column=0, columnspan=2, padx=5,pady=10, sticky="nsew")
+treeviewD.grid(row=10,column=0, columnspan=2, padx=5,pady=10, sticky="nsew")
 
 #Scrollbar vertical
-scroll_y=ttk.Scrollbar(frame_doctores,orient="vertical", command=treeview.yview)
-treeview.configure(yscrollcommand=scroll_y.set)
-scroll_y.grid(row=10,column=2, sticky="ns")
+scroll_yD=ttk.Scrollbar(frame_doctores,orient="vertical", command=treeviewD.yview)
+treeviewD.configure(yscrollcommand=scroll_yD.set)
+scroll_yD.grid(row=10,column=2, sticky="ns")
 
 
 
